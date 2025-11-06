56f3766f12ef44bb9ffc41f7beb687e211768aad
HyeonCheol1211 <guscjf1211@naver.com>
2025-11-06 23:40:24 +0900

    add BOJ14888 and SWEA5215

--- src/SWEA/SWEA5215.java
+++ src/SWEA/SWEA5215.java
@@ -3,7 +3,7 @@ import java.util.*;
 import java.io.*;
 
 public class SWEA5215 {
-    public static void mian(String[] args) throws Exception{
+    public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int T = Integer.parseInt(br.readLine());
         for(int test_case=1;test_case<=T;test_case++){
@@ -13,16 +13,35 @@ public class SWEA5215 {
             int L = Integer.parseInt(st.nextToken());
             int rst;
 
-            int[][] food = new int[20][2];
+            int[][] food = new int[2][N];
             int[] dp = new int[L+1];
+            boolean[][] check = new boolean[L+1][N];
+            dp[0] = 0;
             for(int i=0;i<N;i++){
                 s = br.readLine();
                 st = new StringTokenizer(s);
-                food[i][0] = Integer.parseInt(st.nextToken());
-                food[i][1] = Integer.parseInt(st.nextToken());
+                food[0][i] = Integer.parseInt(st.nextToken());
+                food[1][i] = Integer.parseInt(st.nextToken());
             }
-            
 
+            for(int j=0;j<L;j++){
+                if(j != 0){
+                    if(dp[j] <= dp[j-1]) {
+                        dp[j] = dp[j - 1];
+                        check[j] = check[j - 1].clone();
+                    }
+                }
+
+                for(int i=0;i<N;i++) {
+                    if (food[1][i] + j <= L && !check[food[1][i] + j][i]) {
+                        dp[food[1][i] + j] = dp[j] + food[0][i];
+                        check[food[1][i] + j] = check[j].clone();
+                        check[food[1][i] + j][i] = true;
+                    }
+                }
+            }
+
+            System.out.printf("#%d %d\n", test_case, dp[L]);
 
         }
     }
